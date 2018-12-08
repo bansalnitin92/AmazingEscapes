c30e84368fd1fe11328ceebc900d227112be87ee
Nitin Bansal <nitinbansal@Nitins-MacBook-Air.local>
2018-12-08 20:11:06 +0530

    port

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/images', express.static(__dirname + '/public/images'));
 
-var port = process.env.PORT || 8081;
-
-var server = app.listen(port, function(){
+var server = app.listen(process.env.PORT || '8081', function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });
